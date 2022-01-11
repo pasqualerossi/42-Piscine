4ee9cab5d08e446e0836d45690d934909c0d2458
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 19:53:34 +1030

    Update ft_ultimate_range.c

--- c07/ex02 - ft_ultimate_range/ft_ultimate_range.c
+++ c07/ex02 - ft_ultimate_range/ft_ultimate_range.c
@@ -27,3 +27,25 @@ int	ft_ultimate_range(int **range, int min, int max)
 	}
 	return (i);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int	min;
+	int	max;
+	int	*tab;
+	int	size;
+	int	i = 0;
+
+	min = 5;
+	max = 10;
+	size = ft_ultimate_range(&tab, min, max);
+	while(i < size)
+	{
+		printf("%d, ", tab[i]);
+		i++;
+	}
+
+}
+*/
