9b16313ca2873c2bf40ef8c50abbbf7576c67545
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 18:28:52 +1030

    Update ft_ultimate_div_mod.c

--- c01/ex04 - ft_ultimate_div_mod/ft_ultimate_div_mod.c
+++ c01/ex04 - ft_ultimate_div_mod/ft_ultimate_div_mod.c
@@ -8,3 +8,21 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	*a = div / mod;
 	*b = div % mod;
 }
+/*
+#include <stdio.h>
+
+int		main()
+{
+	int a;
+	int b;
+	
+	a = 5;
+	b = 2;
+
+	printf("a : %d, b : %d\n", a, b);
+
+	ft_ultimate_div_mod(&a, &b);
+
+	printf("a : %d, b : %d\n", a, b);	
+}
+*/
