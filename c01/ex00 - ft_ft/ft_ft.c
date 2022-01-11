3d39bc9d1fd284cd8e3c03a56f5a1f64d1cd83e4
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 18:25:16 +1030

    Update ft_ft.c

--- c01/ex00 - ft_ft/ft_ft.c
+++ c01/ex00 - ft_ft/ft_ft.c
@@ -2,3 +2,20 @@ void	ft_ft(int *nbr)
 {
 	*nbr = 42;
 }
+
+#include <stdio.h>
+
+void ft_ft(int *nbr);
+/*
+int		main(void)
+{
+	int a;
+	int *ptr;
+
+	ptr = &a;
+
+	ft_ft(ptr);
+
+	printf("%d", a);
+}
+*/
