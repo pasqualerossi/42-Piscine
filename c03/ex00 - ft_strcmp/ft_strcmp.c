9319f3074e960256128144099d20328adf9b867c
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 19:12:43 +1030

    Update ft_strcmp.c

--- c03/ex00 - ft_strcmp/ft_strcmp.c
+++ c03/ex00 - ft_strcmp/ft_strcmp.c
@@ -7,3 +7,14 @@ int	ft_strcmp(char *s1, char *s2)
 		i++;
 	return (s1[i] - s2[i]);
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+	printf("%d", ft_strcmp("Hello", "Hello1"));
+	printf("\n%d", ft_strcmp("Hello", "He"));
+	printf("\n%d", ft_strcmp("He", "Hello"));
+	printf("\n%d", ft_strcmp("Hello", "Hello"));
+}
+*/
