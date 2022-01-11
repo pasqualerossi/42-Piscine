02dcf39ea6140ae6ce4f1fb918e552777f906619
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 18:40:20 +1030

    Update ft_str_is_numeric.c

--- c02/ex03 - ft_str_is_numeric/ft_str_is_numeric.c
+++ c02/ex03 - ft_str_is_numeric/ft_str_is_numeric.c
@@ -13,3 +13,14 @@ int	ft_str_is_numeric(char *str)
 	}
 	return (1);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	fputs(ft_str_is_numeric("") ? "true\n" : "false\n", stdout);
+	fputs(ft_str_is_numeric("1211316131618531864987844651") ? "true\n" : "false\n", stdout);
+	fputs(ft_str_is_numeric("11561616818613168681351681616^") ? "true\n" : "false\n", stdout);
+}
+*/
