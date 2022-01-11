3849987a9895cb682127245b9359576231b55bf6
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 15:38:23 +1030

    Update ft_print_comb.c

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -9,26 +9,24 @@ void	ft_print_comb(void)
 	a = '0';
 	while (a <= '7')
 	{
-		b = a;
-		while (++b <= '8')
+		b = a + 1;
+		while (b <= '8')
 		{
 			c = b + 1;
 			while (c <= '9')
 			{
-				write (1, &a, 1);
-				write (1, &b, 1);
-				write (1, &c, 1);
+				write(1, &a, 1);
+				write(1, &b, 1);
+				write(1, &c, 1);
 				if (a != '7' || b != '8' || c != '9')
-				{
-					write (1, ", ", 2);
-				}
+					write(1, ", ", 2);
 				c++;
 			}
+			b++;
 		}
 		a++;
 	}
 }
-
 /*
 int	main(void)
 {
