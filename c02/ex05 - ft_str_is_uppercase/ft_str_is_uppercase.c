32f961fcc3f022be9433766ec46c34e9c68da5e0
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 18:41:58 +1030

    Update ft_str_is_uppercase.c

--- c02/ex05 - ft_str_is_uppercase/ft_str_is_uppercase.c
+++ c02/ex05 - ft_str_is_uppercase/ft_str_is_uppercase.c
@@ -12,3 +12,17 @@ int	ft_str_is_uppercase(char *str)
 	}
 	return (1);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+		
+	fputs(ft_str_is_uppercase("") ? "true\n" : "false\n", stdout);
+	fputs(ft_str_is_uppercase("OISDFUDIUGSDFGJSDJBSDUGJOIASDJSDV") ? "true\n" : "false\n", stdout);
+	fputs(ft_str_is_uppercase("ASDG1") ? "true\n" : "false\n", stdout);
+	fputs(ft_str_is_uppercase("aDSGDFGSDFG") ? "true\n" : "false\n", stdout);
+	fputs(ft_str_is_uppercase("DFHSHFGHDFHTSRHa") ? "true\n" : "false\n", stdout);
+}
+*/
