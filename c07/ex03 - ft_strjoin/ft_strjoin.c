fd061ba900c9f806ba9024666aa580af8605d3ed
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 19:55:13 +1030

    Update ft_strjoin.c

--- c07/ex03 - ft_strjoin/ft_strjoin.c
+++ c07/ex03 - ft_strjoin/ft_strjoin.c
@@ -27,3 +27,28 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	s[c] = '\0';
 	return (s);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int		index;
+	char	**strs;
+	char	*separator;
+	char	*result;
+	int	size;
+
+	size = 3;
+	strs = (char **)malloc(3 * sizeof(char *));
+	strs[0] = (char *)malloc(sizeof(char) * 5 + 1);
+	strs[1] = (char *)malloc(sizeof(char) * 7 + 1);
+	strs[2] = (char *)malloc(sizeof(char) * 14 + 1);
+	strs[0] = "Hello";
+	strs[1] = "friend,";
+	strs[2] = "you are awesome";
+	separator = " ";
+	result = ft_strjoin(size, strs, separator);
+	printf("%s$\n", result);
+	free(result);
+}
+*/
