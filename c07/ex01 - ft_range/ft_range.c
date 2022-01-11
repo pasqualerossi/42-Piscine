8fcf28b6752ba8abd65a366d10a30c11ec7e8a9a
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-11 19:52:13 +1030

    Update ft_range.c

--- c07/ex01 - ft_range/ft_range.c
+++ c07/ex01 - ft_range/ft_range.c
@@ -24,3 +24,26 @@ int	*ft_range(int min, int max)
 	}
 	return (result);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int	min;
+	int	max;
+	int	*tab;
+	int	i = 0;
+	int	size;
+
+	min = 5;
+	max = 10;
+	size = max - min;
+	tab = ft_range(min, max);
+	while(i < size)
+	{
+		printf("%d, ", tab[i]);
+		i++;
+	}
+
+}
+*/
