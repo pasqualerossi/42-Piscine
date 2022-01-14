7c6d713f6644f7d4f3f18ede56eb381f6f81bb4b
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-14 10:31:05 +1030

    Update ft_print_alphabet.c

--- c00/ex01 - ft_print_alphabet/ft_print_alphabet.c
+++ c00/ex01 - ft_print_alphabet/ft_print_alphabet.c
@@ -1,12 +1,27 @@
 #include <unistd.h>
 
-void	ft_print_alphabet()
+void	ft_print_alphabet(void)
 {
-	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
-}
+	char	count;
 
+	count = 97;
+	while (count >= 97 && count <= 122)
+	{
+		write(1, &count, 1);
+		count++;
+	}
+/*
 int	main(void)
 {
 	ft_print_alphabet();
 	return (0);
+}	
+	
+//second version 
+#include <unistd.h>
+
+void	ft_print_alphabet()
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
+*/
