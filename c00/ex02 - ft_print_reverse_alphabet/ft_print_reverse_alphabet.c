82f29ccb09bc60dce17a593d199960994f9681ec
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-14 10:37:03 +1030

    Update ft_print_reverse_alphabet.c

--- c00/ex02 - ft_print_reverse_alphabet/ft_print_reverse_alphabet.c
+++ c00/ex02 - ft_print_reverse_alphabet/ft_print_reverse_alphabet.c
@@ -9,11 +9,17 @@ void	ft_print_reverse_alphabet(void)
 		write(1, &count, 1);
 }
 /*
+
+//main to run ft_print_reserve_alphabet
+//delete this int main before submitting project
+
 int main(void)
 {
 	ft_print_reverse_alphabet();
 	return (0);
 }
+
+//this is a second and shorter version of ft_print_reserve_alphabet
 
 void	ft_print_reverse_alphabet(void)
 {
