cb11ca636c75b3103f89b6139d6b38b1b08026ff
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-01-26 15:53:27 +1030

    Update ft_str_is_numeric.c

--- C00 - C07 Piscine Projects/c02/ex03 - ft_str_is_numeric/ft_str_is_numeric.c
+++ C00 - C07 Piscine Projects/c02/ex03 - ft_str_is_numeric/ft_str_is_numeric.c
@@ -19,19 +19,13 @@ int	ft_str_is_numeric(char *str)
 int main()
 {
     int result;
-
-    result = ft_str_is_numeric("asdf");
-    printf("Result: %d\n", result);
-
+    
     result = ft_str_is_numeric("QWERTY");
     printf("Result: %d\n", result);
-
-    result = ft_str_is_numeric("341");
-    printf("Result: %d\n", result);
-
+    
     result = ft_str_is_numeric("000999");
     printf("Result: %d\n", result);
-
+    
     result = ft_str_is_numeric("abcd1234");
     printf("Result: %d\n", result);
     
