f5ab78d8539af2677cc25588be62b7d8bb33ff47
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-05 17:51:21 +1030

    Create alpha_mirror.c

---
+++ 42 Piscine Exam/Exam Answers/Level 2/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	char	ltr;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			ltr = argv[1][i];
+			if ('A' <= argv [1][i] && 'Z' >= argv[1][i])
+				ltr = 'Z' - argv[1][i] + 'A';
+			if ('a' <= argv[1][i] && 'z' >= argv[1][i])
+				ltr = 'z' -argv[1][i] + 'a';
+			write(1, &ltr, 1);
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
