31c30d6e9ef4679198e02dda0e4ca72c8eac87d5
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-05 17:49:10 +1030

    Create last_word.c

---
+++ 42 Piscine Exam/Exam Answers/Level 2/last_word/last_word.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void	last_word(char *str)
+{
+	int	j;
+	int i;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] >= 33 && str[i + 1] <= 126)
+			j = i + 1;
+		i++;
+	}
+	while (str[j] >= 33 && str[j] <= 127)
+	{
+		write(1, &str[j], 1);
+		j++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+		last_word(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
