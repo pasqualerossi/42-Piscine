c0a78f1c6f89298ecb182bcbd3a18e3191f6ea03
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-05 17:48:34 +1030

    Create inter.c

---
+++ 42 Piscine Exam/Exam Answers/Level 2/inter/inter.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int	iter(char *str, char c, int len)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && (i < len || len == -1))
+		if (str[i++] == c)
+			return (1);
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc == 3)
+	{
+		i = 0;
+		while (argv[1][i])
+		{
+			if (!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
+				write(1, &argv[1][i], 1);
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
