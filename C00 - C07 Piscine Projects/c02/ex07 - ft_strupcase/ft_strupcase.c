cc0cef4dfcc5abd0bd23c621ef263b3e52245602
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-05 18:26:29 +1030

    Update ft_strupcase.c

--- C00 - C07 Piscine Projects/c02/ex07 - ft_strupcase/ft_strupcase.c
+++ C00 - C07 Piscine Projects/c02/ex07 - ft_strupcase/ft_strupcase.c
@@ -16,12 +16,9 @@ char	*ft_strupcase(char *str)
 
 int main(void)
 {
-	char str[] = "MesCouidlls au b0rd de l0$%^";
-
-	printf("%s\n", ft_strupcase(str));
-	//printf("%s\n", ft_strupcase("ajsfoiajSHSHFUDFUcjzojdfijsd15568s4df?"));
-	//printf("%s\n", ft_strupcase("JNDVDHVDUFIVDUFVIODVNXZNVNDB"));
-	//printf("%s\n", ft_strupcase("5116468615168135132100"));
-	//printf("%s\n", ft_strupcase("%^*&*()%$#$%^&*(*(&*(&^%*&$^%$%)))"));
-}
+	char a[] = "fhjdhfjhfjhfkjdsf";
+	
+	printf("%s\n", ft_strupcase(a));
+	return (0);
+} 
 */
