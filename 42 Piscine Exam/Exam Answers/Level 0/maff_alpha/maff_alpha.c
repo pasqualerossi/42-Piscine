9a38cf9f175a311af720c7d5737e46d001fd6706
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-05 16:29:03 +1030

    Update maff_alpha.c

--- 42 Piscine Exam/Exam Answers/Level 0/maff_alpha/maff_alpha.c
+++ 42 Piscine Exam/Exam Answers/Level 0/maff_alpha/maff_alpha.c
@@ -1,26 +1,7 @@
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int   main(void)
 {
-	int i;
-
-	i = 0;
-	if (argc != 2)
-	{
-		write(1, "a\n", 1);
-	}
-	else 
-	{
-		while (argv[1][i])
-		{
-			if (argv[1][i] == 'a')
-			{
-				write(1, "a", 1);
-				break;
-			}
-			i += 1;
-		}
-		write(1, "\n", 1);
-		return (0);
-	}
+    write(1,"aBcDeFgHiJkLmNoPqRsTuVwXyZ\n", 27);
+    return (0);
 }
