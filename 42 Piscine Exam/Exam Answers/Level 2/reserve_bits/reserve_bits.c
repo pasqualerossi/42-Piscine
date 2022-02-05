889c0e997aa7f3b0cff75b743a1ded0739faf68b
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-05 17:49:45 +1030

    Create reserve_bits.c

---
+++ 42 Piscine Exam/Exam Answers/Level 2/reserve_bits/reserve_bits.c
@@ -0,0 +1,13 @@
+unsigned char	reverse_bits(unsigned char octet)
+{
+	unsigned char	res = 0;
+	int i = 8;
+
+	while (i > 0)
+	{
+		res = res * 2 + (octet % 2);
+		octet = octet / 2;
+		i--;
+	}
+	return (res);
+}
