ab4228316f9e03aa5fe0fcd0050032d613ccaa1f
Christina Rossi <82299698+Tiney1001@users.noreply.github.com>
2022-02-06 12:48:17 +1030

    Update ft_strjoin.c

--- C00 - C07 Piscine Projects/c07/ex03 - ft_strjoin/ft_strjoin.c
+++ C00 - C07 Piscine Projects/c07/ex03 - ft_strjoin/ft_strjoin.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
@@ -28,27 +29,13 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	return (s);
 }
 /*
-#include <stdio.h>
-
-int main(void)
+int	main(void)
 {
-	int		index;
-	char	**strs;
-	char	*separator;
-	char	*result;
-	int	size;
-
-	size = 3;
-	strs = (char **)malloc(3 * sizeof(char *));
-	strs[0] = (char *)malloc(sizeof(char) * 5 + 1);
-	strs[1] = (char *)malloc(sizeof(char) * 7 + 1);
-	strs[2] = (char *)malloc(sizeof(char) * 14 + 1);
-	strs[0] = "Hello";
-	strs[1] = "friend,";
-	strs[2] = "you are awesome";
-	separator = " ";
-	result = ft_strjoin(size, strs, separator);
-	printf("%s$\n", result);
-	free(result);
-}
-*/
+	char	*tab[4];
+	tab[0] = " ";
+	tab[1] = " ";
+	tab[2] = " ";
+	tab[3] = " ";
+	printf("%s", ft_strjoin(4, tab, "Pasquale"));
+	return (0);
+}*/
