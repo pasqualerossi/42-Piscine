675478365f305297a8fca0bf036c95b87e2689d8
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-02-09 17:14:14 +1030

    Create rush00.c

---
+++ Rush00, Rush01 and Rush02/Rush00 - Rectange (X and Y)/rush00.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	print_line(char start, char middle, char end, int column_count)
+{
+	ft_putchar(start);
+	while (--column_count > 1)
+		ft_putchar(middle);
+	if (column_count)
+		ft_putchar(end);
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	if (x > 0 && y > 0)
+	{
+		i = 0;
+		while (i < y)
+		{
+			if (i == 0)
+				print_line('o', '-', 'o', x);
+			else if (i == y - 1)
+				print_line('o', '-', 'o', x);
+			else
+				print_line('|', ' ', '|', x);
+			i++;
+			write(1, "\n", 1);
+		}
+	}
+}
