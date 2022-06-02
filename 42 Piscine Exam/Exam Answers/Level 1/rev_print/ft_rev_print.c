c297743f2728000fd024fa62650c20a16ac11c58
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-02 14:19:29 +0930

    Update ft_rev_print.c

--- 42 Piscine Exam/Exam Answers/Level 1/rev_print/ft_rev_print.c
+++ 42 Piscine Exam/Exam Answers/Level 1/rev_print/ft_rev_print.c
@@ -1,16 +1,15 @@
 #include <unistd.h>
 
-int	main(int ac, char *av[])
+int	main(int argc, char *argv[])
 {
-	int	i;
+	int	i = 0;
 
-	if (ac == 2)
+	if (argc == 2)
 	{
-		i = 0;
-		while (av[1][i])
+		while (argv[1][i])
 			i += 1;
 		while (i)
-			write(1, &av[1][--i], 1);
+			write(1, &argv[1][--i], 1);
 	}
 	write(1, "\n", 1);
 	return (0);
