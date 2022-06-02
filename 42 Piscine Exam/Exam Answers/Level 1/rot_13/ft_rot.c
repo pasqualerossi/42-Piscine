cffdcfc3f983d0c30307e58f464577eb0e7bf406
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-02 14:29:46 +0930

    Update ft_rot.c

--- 42 Piscine Exam/Exam Answers/Level 1/rot_13/ft_rot.c
+++ 42 Piscine Exam/Exam Answers/Level 1/rot_13/ft_rot.c
@@ -1,19 +1,22 @@
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
-	int	i;
+	int	i = 0;
 
-	if (ac > 1)
+	if (argc > 1)
 	{
-		i = 0;
-		while(av[1][i])
+		while(argv[1][i])
 		{
-			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				av[1][i] = (av[1][i] - 'a' + 13) % 26 + 'a';
-			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-					av[1][i] = (av[1][i] - 'A' + 13) % 26 + 'A';
-			write(1, &av[1][i], 1);
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+			{
+				argv[1][i] = (argv[1][i] - 'a' + 13) % 26 + 'a';
+			}
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			{
+					argv[1][i] = (argv[1][i] - 'A' + 13) % 26 + 'A';
+			}
+			write(1, &argv[1][i], 1);
 			i++;
 		}
 	}
