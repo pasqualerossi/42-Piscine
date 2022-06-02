410180ed1a91536183aa26acb5019e9b851c99f3
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-02 12:39:42 +0930

    Update ft_putstr.c

--- 42 Piscine Exam/Exam Answers/Level 1/ft_putstr/ft_putstr.c
+++ 42 Piscine Exam/Exam Answers/Level 1/ft_putstr/ft_putstr.c
@@ -4,14 +4,13 @@ void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-
 void	ft_putstr(char *str)
 {
 	int i = 0;
-
-	while (*(str + i) != '\0')
+	
+	while (str[i])
 	{
-		ft_putchar(*(str + i));
+		ft_putchar(str[i]);
 		i++;
 	}
 }
