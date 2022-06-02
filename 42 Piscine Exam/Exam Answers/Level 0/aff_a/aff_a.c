11b562648e8caeb7b63273e2d438bb00167ed12c
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-02 12:00:18 +0930

    Update aff_a.c

--- 42 Piscine Exam/Exam Answers/Level 0/aff_a/aff_a.c
+++ 42 Piscine Exam/Exam Answers/Level 0/aff_a/aff_a.c
@@ -1,27 +1,23 @@
 #include <unistd.h>
 
-int		main(int argc, char *argv[])
+int		main(int argc, char **argv)
 {
-	int		i;
+	int i = 0;
 
-	i = 0;
-	if (argc != 2)
+	if (argc == 2)
 	{
-		write(1, "a\n", 2);
-		return (0);
-	}
-	else
-	{
-		while (argv[1][i])
+		while (argv[1][i] != '\0')
 		{
 			if (argv[1][i] == 'a')
 			{
-				write(1, "a", 1);
-				break ;
+				write(1, "a\n", 2);
+				return (0);
 			}
-			i += 1;
+			i++;
 		}
 		write(1, "\n", 1);
 		return (0);
 	}
+	write(1, "a\n", 2);
+	return (0);
 }
