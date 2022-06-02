b67e77bd52a243320bdb269c32d0450bbcd7be62
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-02 12:01:11 +0930

    Update aff_first_param.c

--- 42 Piscine Exam/Exam Answers/Level 0/aff_first_param/aff_first_param.c
+++ 42 Piscine Exam/Exam Answers/Level 0/aff_first_param/aff_first_param.c
@@ -1,21 +1,12 @@
 #include <unistd.h>
 
-int	main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int	i;
+	int i = -1;
 
-	i = 0;
-	if (argc < 2)
-	{
-		write(1, "\n", 1);
-	}
-	else
-	{
-		while (argv[1][i])
-		{
-			write(1, &argv[1][i++], 1);
-		}
-		write(1, "\n", 1);
-	}
+	if (argc > 1)
+		while (argv[1][++i])
+			write(1, &argv[1][i], 1);
+	write(1, "\n", 1);
 	return (0);
 }
