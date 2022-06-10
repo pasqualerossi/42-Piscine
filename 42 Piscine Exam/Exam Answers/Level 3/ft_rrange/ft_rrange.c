31af0dbfba06c8bc792c8d1b0547dd1511b1cd87
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-10 13:51:52 +0930

    Create ft_rrange.c

---
+++ 42 Piscine Exam/Exam Answers/Level 3/ft_rrange/ft_rrange.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+int *ft_rrange(int start, int end)
+{
+	int *rrange;
+	int i = 0;
+
+	if (start > end)
+		rrange = (int *)malloc(sizeof(int) * (start - end) + 1);
+	else
+		rrange = (int *)malloc(sizeof(int) * (end - start) + 1);
+	while (start != end)
+	{
+		rrange[i++] = end;
+		end -= (start > end) ? -1 : 1;
+	}
+	rrange[i] = end;
+	return (rrange);
+}
