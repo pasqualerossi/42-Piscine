b635b5b5c3c482621c2a7eccfa55a1f7e2e73ce5
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-10 14:01:24 +0930

    Create tab_mult.c

---
+++ 42 Piscine Exam/Exam Answers/Level 3/tab_mult/tab_mult.c
@@ -0,0 +1,69 @@
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int result = 0;
+	int sign;
+
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = (nb % 1000000000 * -1);
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = (nb * -1);
+	}
+	if (nb / 10 > 0)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + '0');
+}
+
+int main(int argc, char *argv[])
+{
+	int	i;
+	int	nbr;
+
+	if (argc != 2)
+		write(1, "\n", 1);
+	else
+	{
+		i = 1;
+		nbr = ft_atoi(argv[1]);
+		while (i <= 9)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(nbr);
+			write(1, " = ", 3);
+			ft_putnbr(i * nbr);
+			write(1, "\n", 1);
+			i += 1;
+		}
+	}
+	return (0);
+}
