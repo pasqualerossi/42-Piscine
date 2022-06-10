ba07e322d89e9d86efaeeb21c0b5efdefd090eeb
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-10 13:39:57 +0930

    Create epur_str.c

---
+++ 42 Piscine Exam/Exam Answers/Level 3/epur_str/epur_str.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+int	 main(int argc, char const *argv[])
+{
+	int i;
+	int flg;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i += 1;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+				flg = 1;
+			if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+			{
+				if (flg)
+					write(1, " ", 1);
+				flg = 0;
+				write(1, &argv[1][i], 1);
+			}
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
