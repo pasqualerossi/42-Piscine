c5c1e09cb93b5dc90581458041c490869a715d5a
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-06-10 13:59:53 +0930

    Create str_capitalizer.c

---
+++ 42 Piscine Exam/Exam Answers/Level 3/str_capitalizer/str_capitalizer.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void	str_capitalizer(char *str)
+{
+	int		i = 0;
+
+	if (str[i] >= 'a' && 'z' >= str[i])
+		str[i] -= 32;
+	write(1, &str[i], 1);
+	while (str[++i])
+	{
+		if (str[i] >= 'A' && 'Z' >= str[i])
+			str[i] += 32;
+		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || \
+		str[i] == '\t'))
+			str[i] -= 32;
+		write(1, &str[i], 1);
+	}
+}
+
+int		main(int argc, char *argv[])
+{
+	int		i;
+
+	if (argc < 1)
+		write(1, "\n", 1);
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			str_capitalizer(argv[i]);
+			write(1, "\n", 1);
+			i += 1;
+		}
+	}
+	return (0);
+}
